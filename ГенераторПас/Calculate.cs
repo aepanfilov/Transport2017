7746631c1a012257177894c3e1e122cdbfe007d8
Sereodas <mr.sergey.shekhovtsov@mail.ru>
2021-04-15 22:56:08 +0300

    Добавил матрицы

--- ГенераторПас/Calculate.cs
+++ ГенераторПас/Calculate.cs
@@ -17,17 +17,16 @@ namespace Транспорт2017.ГенераторПас
             FileInfo file = new FileInfo("данные\\1.xlsx");
             using (ExcelPackage package = new ExcelPackage(file))
             {
-                ExcelWorksheet wkSheet = package.Workbook.Worksheets["Маршруты"];
-                //int КолОстановок = wkSheet.Cells[1, 1].GetValue<int>();
+                ExcelWorksheet routes = package.Workbook.Worksheets["Маршруты"];
                 int x = 2;
                 int codeStop;
                 do
                 {
-                    codeStop = wkSheet.Cells[x, 1].GetValue<int>();
-                    string nameStop = wkSheet.Cells[x, 2].GetValue<string>();
-                    string district = wkSheet.Cells[x, 3].GetValue<string>();
-                    int countPass = wkSheet.Cells[x, 4].GetValue<int>();
-                    int attraction = wkSheet.Cells[x, 5].GetValue<int>();
+                    codeStop = routes.Cells[x, 1].GetValue<int>();
+                    string nameStop = routes.Cells[x, 2].GetValue<string>();
+                    string district = routes.Cells[x, 3].GetValue<string>();
+                    int countPass = routes.Cells[x, 4].GetValue<int>();
+                    int attraction = routes.Cells[x, 5].GetValue<int>();
                     if (codeStop != 0) { 
                         listStop.Add(
                         new Stop { CodeStop = codeStop, NameStop = nameStop, District = district, CountPass = countPass, Attraction = attraction }
@@ -38,7 +37,95 @@ namespace Транспорт2017.ГенераторПас
                 }
                 while (codeStop != 0);
 
+                ExcelWorksheet population = package.Workbook.Worksheets["Население"];
 
+                x = 0; // строки матрицы
+                int y = 0;  // столбцы матрицы
+                int N = 8; // размерность матрицы
+                int M = 15; // размерность матрицы доли пассажиров, отъезжающих от остановки, в зависимости от времени прибытия
+
+                double[,] mornWork = new double[N, N]; // матрица доли рабочих и молодёжи утром
+                double[,] mornPens = new double[N, N]; // матрица доли пенсионеров и школьников утром
+                double[,] dayTime = new double[N, N]; // матрица доли людей в обеденное время
+                double[,] evenWork = new double[N, N]; // матрица доли рабочих и молодёжи вечером
+                double[,] evenPens = new double[N, N]; // матрица доли пенсионеров и школьников вечером
+                double[,] timeDist = new double[N, M]; // матрица доли пассажиров, отъезжающих от остановки, в зависимости от времени прибытия
+
+                for (int i = 160; i < N + 160; i++)
+                {
+                    for (int j = 3; j < N + 3; j++)
+                    {
+                        mornWork[x, y] = population.Cells[i, j].GetValue<double>();
+                        y++;
+                    }
+                    y = 0;
+                    x++;
+                }
+
+                x = 0;
+
+                for (int i = 173; i < N + 173; i++)
+                {
+                    for (int j = 3; j < N + 3; j++)
+                    {
+                        mornPens[x, y] = population.Cells[i, j].GetValue<double>();
+                        y++;
+                    }
+                    y = 0;
+                    x++;
+                }
+
+                x = 0;
+
+                for (int i = 194; i < N + 194; i++)
+                {
+                    for (int j = 3; j < N + 3; j++)
+                    {
+                        dayTime[x, y] = population.Cells[i, j].GetValue<double>();
+                        y++;
+                    }
+                    y = 0;
+                    x++;
+                }
+
+                x = 0;
+
+                for (int i = 207; i < N + 207; i++)
+                {
+                    for (int j = 3; j < N + 3; j++)
+                    {
+                        evenWork[x, y] = population.Cells[i, j].GetValue<double>();
+                        y++;
+                    }
+                    y = 0;
+                    x++;
+                }
+
+                x = 0;
+
+                for (int i = 207; i < N + 207; i++)
+                {
+                    for (int j = 15; j < N + 15; j++)
+                    {
+                        evenPens[x, y] = population.Cells[i, j].GetValue<double>();
+                        y++;
+                    }
+                    y = 0;
+                    x++;
+                }
+
+                x = 0;
+
+                for (int i = 220; i < N + 220; i++)
+                {
+                    for (int j = 3; j < M + 3; j++)
+                    {
+                        timeDist[x, y] = population.Cells[i, j].GetValue<double>();
+                        y++;
+                    }
+                    y = 0;
+                    x++;
+                }
             }
         }
     }
